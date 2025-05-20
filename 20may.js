e82f1fa8c2c223d8fb16a04e8563d75582725e91
kritika-mudgil <kritikamudgil10@gmail.com>
2025-05-20 11:44:01 +0530

    Add files via upload

--- 20may.js
+++ 20may.js
@@ -3,3 +3,15 @@ const obj={name:"kritika",city:"Faridabad",age:23};
 console.log(obj.name)
 console.log(obj.city)
 console.log(obj.age)
+
+const obj2=new Object();
+obj2.college="ABES";
+obj2.name="Kritika";
+
+console.log(obj2)
+console.log(obj2["college"])
+delete obj.city
+console.log(obj)
+const A="city"
+obj[A]="Faridabad";
+console.log(obj)
